60f67c8729d190797d8407e9d33974b393863e36
Dannee Kim <kim.dannee@gmail.com>
2019-07-07 15:07:50 -0400

    Made Films API calls Async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,47 +14,64 @@ export class AppComponent {
   films = <any>[];
   filmsLoaded: boolean = false;
 
-  constructor( private characterService: CharacterService ){}
+  constructor(private characterService: CharacterService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getCharacters();
   }
 
-  getCharacters(){
+  getCharacters() {
     this.characterService.getCharacterDetails().subscribe(
       (data) => {
-        this.charactersJSON$=data;
+        this.charactersJSON$ = data;
         console.log(this.charactersJSON$);
-      }
-  )}
-
-
-  getCharacterInfo(url){
-    console.log(url);
-    this.characterService.getUrl(url).subscribe(
-      (data) => {
-        this.characterInfo$ = data;
-        console.log(this.characterInfo$)
-        this.characterInfo$.films.forEach(filmUrl => {
-          // console.log(film)
-          this.getMovies(filmUrl);
-        });
       }
     )
   }
 
-  getMovies(url){
-    this.films = [];
-    this.characterService.getUrl(url).subscribe(
-      (data) => {
-        this.films.push(data);
-      },
-      (error) => {
-        console.log(error)
-      },
-      () => {
-        console.log(this.films)
-      })
+  asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array)
+    }
   }
 
+  getCharacterInfo(url, name) {
+    this.films = [];
+    var temp = [];
+    console.log(url);
+
+    if (name === 'Obi-Wan Kenobi') {
+      //make API call to people API to get the list of films for Obi-Wan Kenobi
+    }
+    else {  
+      this.characterService.getUrl(url).subscribe(
+        async data => {
+         this.characterInfo$ = data;
+ 
+         const start = async () => {
+           await this.asyncForEach(this.characterInfo$.films, async (film) => {
+             temp.push(await this.getMovies(film));
+           });
+         }
+         await start();
+         this.films = temp;
+       }
+     )
+    }
+  }
+
+  getMovies(url) {
+    return new Promise(resolve => {
+      let films = {};
+      this.characterService.getUrl(url).subscribe(
+        (data) => {
+          resolve(data);
+        },
+        (error) => {
+          console.log(error)
+        },
+        () => {
+        })
+    })
+  }
 }
