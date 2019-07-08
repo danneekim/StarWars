7854c907a6ec169e0f7c662e947cf339fdb85f68
Dannee Kim <kim.dannee@gmail.com>
2019-07-08 05:34:41 -0400

    Initial Heroku commit

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const path = require('path');
 app.use (express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
+
+// PathLocationStrategy
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Listening!')
